b9f2f0ab11b7483360ea6eff9b63c355e3bfe7a9
Marco <marcof3900@gmail.com>
2026-03-23 15:12:45 -0600

    Commit 3: Refined Monthly Average Data Table

--- Analysis.py
+++ Analysis.py
@@ -18,5 +18,23 @@ print(f"\nAverage Daily Calories: {avg_cals:.0f} cal")
 # - Convert Date to type datetime
 df['Date'] = pd.to_datetime(df['Date'])
 
-monthly_avg = df.groupby(df['Date'].dt.month)[[
-    'Calories', 'Protein', 'Carbohydrate', 'Fat']].mean()
+# - Get month name from Date
+df['Month'] = df['Date'].dt.month_name()
+
+# - Define order of months
+month_order = ["January", "February", "March"]
+# - Define Nutrition categories
+nutrition_categories = ["Calories", "Protein", "Carbohydrate", "Fat"]
+
+# - Group by months and get mean
+# -- .dt.month to get month from Date
+monthly_avg = df.groupby('Month')[['Calories', 'Protein', 'Carbohydrate', 'Fat']].mean().reset_index()
+
+# - Order months
+monthly_avg['Month'] = pd.Categorical(monthly_avg['Month'], categories=month_order, ordered=True)
+monthly_avg = monthly_avg.sort_values('Month').reset_index(drop=True)
+
+# - Round averages to whole number
+monthly_avg[nutrition_categories] = monthly_avg[nutrition_categories].round(0).astype(int)
+
+print(monthly_avg)
